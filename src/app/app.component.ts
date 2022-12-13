6005468f5fd437e25dd44e1e39d850cbe2f213e7
tienbku <tien.ngtruong@gmail.com>
2022-12-13 15:46:37 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-15-crud';
+  title = 'Angular 15 CRUD example';
 }
